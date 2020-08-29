2cda9264615d6160cc9aab8fad699299e0553069
Christopher Harrison <chrhar@microsoft.com>
2020-08-29 16:24:08 -0700

    Added Azure Functions

--- api/dogs/index.js
+++ api/dogs/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('Returning list of dogs.');
     const dogs = [
+        {name: 'Azure'},
         {name: 'Sammy'},
         {name: 'Roscoe'},
         {name: 'Bailey'},
